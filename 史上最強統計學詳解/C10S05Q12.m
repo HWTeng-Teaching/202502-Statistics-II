dbc03fb9d29cd975a4b4a092b436c04d47dbe3a9
FrankHsu-IMF <pyhsu168@gmail.com>
2025-03-17 09:36:56 +0800

    Create C10S05Q12.m

---
+++ 史上最強統計學詳解/C10S05Q12.m
@@ -0,0 +1,12 @@
+![image](https://github.com/user-attachments/assets/8a3b763e-b5cc-49d5-b076-a7c732ab6004)
+![image](https://github.com/user-attachments/assets/f7d92974-7d23-4f09-bdf3-a5a9e90a0a84)
+#### a.
+Completions\
+$\bar{x} = \frac{\sum x_i}{n} = \frac{350}{16} = 21.25$
+
+$S_x = \sqrt{\frac{\sum (x_i - \bar{x})^2}{n - 1}} = 2.316$
+
+Yards per pass\
+$\bar{y} = \frac{\sum y_i}{n} = \frac{207.8}{16} = 12.99$
+
+$S_y = \sqrt{\frac{\sum (y_i - \bar{y})^2}{n - 1}} = 2.52$
